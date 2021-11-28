bbbe9b75c8cadd4325d6697f22dd7bc8776ead2b
Daniel <danielrs9504@gmail.com>
2021-11-28 06:12:24 -0500

    Renderiza el componente Home desde el servidor

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 import express from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
+import { Home } from './src/pages/Home';
 
 const app = express();
+
+// Por defecto si express esta sirviendo una carpeta estatica y ve que hay un index, y que el usuario esta intentando cargar data de el, inmediatamente envia el index
+app.use(express.static('./build', { index: false }));
 
 // Todas las rutas devolvera el HTML
 app.get('/*', (req, res) => {
   // render es una funcion a la que le podemos pasar jsx y lo renderizara como html
-  const reactApp = renderToString(<h1>Hello from the server side</h1>);
+  const reactApp = renderToString(<Home />);
 
   return res.send(`
     <html>
